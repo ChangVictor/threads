b0bd7013bca0d6675c7f920a0e87c5aa0dcfb31b
ChangVictor <v.chang.yu@gmail.com>
2024-04-16 20:43:59 +0200

    initial commit

---
+++ Threads/Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Victor Chang on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
